e1de334d290bf7dc84110c1dce1d5e7a1c497b4f
PriscilaNC <p.navarrete06@ufromail.cl>
2024-07-04 00:14:04 -0400

    index para iniciar la pagina

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,54 @@
 <template>
-    <div>
-      <h1>Nada que ver aqui :)</h1>
+  <div class="home-page">
+    <div class="overlay">
+      <img src="~/assets/logo-login.png" alt="GreenBinGuard Logo" class="logo">
+      <nuxt-link to="/login"><button class="login-button">Ir al login / registro</button></nuxt-link>
     </div>
-  </template>
-  
-  <script>
+  </div>
+</template>
 
-  
-  export default {
-   
-  }
-  </script>
-  
-  <style>
-  body {
-    font-family: Arial, sans-serif;
-    margin: 0;
-    padding: 0;
-  }
-  </style>
-  
+<script setup>
+</script>
+
+<style scoped>
+.home-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: url('~/assets/background2.jpg') no-repeat center center;
+  background-size: cover;
+  position: relative;
+}
+
+.overlay {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  border-radius: 10px;
+}
+
+.logo {
+  width: 200px;
+  height: auto;
+  margin-bottom: 20px;
+}
+
+.login-button {
+  background-color: #7ec041;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+  text-decoration: none;
+  text-align: center;
+  transition: background-color 0.3s;
+}
+
+.login-button:hover {
+  background-color: #6abf37;
+}
+</style>
